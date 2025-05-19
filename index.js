980b8c550528e234832fb004e96f5f42c9b14d12
David Arrebola <arrebola.work@gmail.com>
2025-05-19 20:08:16 +0200

    base de datos creada correctamente

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
+const dotenv = require("dotenv").config();
 app.use(express.json());
 const PORT = 3000;
+
+const db = mysql.createConnection({ host: "localhost", user: "root", password: process.env.DB_PASS });
+db.connect();
+
+app.get("/createDB", (req, res) => {
+  const sqlString = "CREATE DATABASE tiendaTestDb";
+  db.query(sqlString, (err, result) => {
+    if (err) throw err;
+    res.status(201).send("Database created successfully");
+  });
+});
 
 app.listen(PORT, () => console.log(`Servidor levantado en el Puerto ${PORT}`));
